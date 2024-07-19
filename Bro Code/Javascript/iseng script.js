12cd6078b79163cef7fbd7724c2ccce285a72d0f
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-07-19 16:58:01 +0700

    small update

--- Bro Code/Javascript/iseng script.js
+++ Bro Code/Javascript/iseng script.js
@@ -1,11 +1,15 @@
 let x = 2;
 const y = 10;
 const multiplier = 10;
+const reduction = 2;
 
 result = x + y;
 multipliedResult = result * multiplier;
 
+reducedResult = result - reduction;
+
 console.log(result);
 console.log(multipliedResult);
+console.log(reducedResult);
 
 console.log("Hello World");
