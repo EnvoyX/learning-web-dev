fb5373ee5469be60ff7f45eccfe2af614074742e
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-10-05 00:45:02 +0700

    script of Ngobar-27 (dark-light mode feature) has been worked as intended

--- WPU/CSS/CSS Bootstrap/Ngobar-27-Website-Portofolio/script.js
+++ WPU/CSS/CSS Bootstrap/Ngobar-27-Website-Portofolio/script.js
@@ -44,13 +44,11 @@
     }
 
     const themeSwitcherText = document.querySelector('#bd-theme-text');
-    const activeThemeIcon = document.querySelector('.theme-icon-active use');
+    const activeThemeIcon = document.querySelector('.theme-icon-active');
     const btnToActive = document.querySelector(
       `[data-bs-theme-value="${theme}"]`
     );
-    const svgOfActiveBtn = btnToActive
-      .querySelector('svg use')
-      .getAttribute('href');
+    const iconOfActiveBtn = btnToActive.querySelector('i').dataset.themeIcon;
 
     document.querySelectorAll('[data-bs-theme-value]').forEach((element) => {
       element.classList.remove('active');
@@ -59,7 +57,9 @@
 
     btnToActive.classList.add('active');
     btnToActive.setAttribute('aria-pressed', 'true');
-    activeThemeIcon.setAttribute('href', svgOfActiveBtn);
+    activeThemeIcon.classList.remove(activeThemeIcon.dataset.themeIconActive);
+    activeThemeIcon.classList.add(iconOfActiveBtn);
+    activeThemeIcon.dataset.iconActive = iconOfActiveBtn;
     const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
     themeSwitcher.setAttribute('aria-label', themeSwitcherLabel);
 
