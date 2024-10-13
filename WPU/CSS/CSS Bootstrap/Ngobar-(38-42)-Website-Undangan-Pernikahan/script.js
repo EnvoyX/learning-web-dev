4f52f03f26a5d35bc7112c0c896ea95da6b55c7e
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-10-13 18:17:32 +0700

    added feature to disable scrolling and smooth scrolling
    enable it after the user click "Lihat Undangan" and check whether the user have visited the website by adding a localStorage so the user don't have to click the button again if the user refreshes the page

--- WPU/CSS/CSS Bootstrap/Ngobar-(38-42)-Website-Undangan-Pernikahan/script.js
+++ WPU/CSS/CSS Bootstrap/Ngobar-(38-42)-Website-Undangan-Pernikahan/script.js
@@ -12,3 +12,29 @@ const btnClose = document.querySelector('.btn-close');
 offcanvas.addEventListener('hidden.bs.offcanvas', function () {
   stickyTop.style.overflow = 'hidden';
 });
+
+// Enable & Disable Scroll
+const rootElement = document.querySelector(':root');
+// Disable Scroll at landing page
+function disableScroll() {
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  window.onscroll = function () {
+    window.scrollTo(scrollTop, scrollLeft);
+  };
+  // Disable Smooth Scrolling from Bootstrap
+  rootElement.style.scrollBehavior = 'auto';
+}
+
+// Enable Scroll & smooth scrolling after click "lihat undangan"
+function enableScroll() {
+  window.onscroll = function () {
+    rootElement.style.scrollBehavior = 'smooth';
+    localStorage.setItem('opened', 'true');
+  };
+}
+
+if (!localStorage.getItem('opened')) {
+  disableScroll();
+}
