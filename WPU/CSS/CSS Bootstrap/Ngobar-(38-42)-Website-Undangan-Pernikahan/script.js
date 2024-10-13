44ec2229e2cf95ced3da0a9112e9a955e3987d34
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-10-13 23:13:07 +0700

    added RSVP and gifts page, also added footer

--- WPU/CSS/CSS Bootstrap/Ngobar-(38-42)-Website-Undangan-Pernikahan/script.js
+++ WPU/CSS/CSS Bootstrap/Ngobar-(38-42)-Website-Undangan-Pernikahan/script.js
@@ -38,3 +38,19 @@ function enableScroll() {
 if (!localStorage.getItem('opened')) {
   disableScroll();
 }
+
+//Submit Event
+window.addEventListener('load', function () {
+  const form = document.getElementById('my-form');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    }).then(() => {
+      alert('Success!');
+    });
+  });
+});
