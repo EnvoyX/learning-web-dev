4053f997a6d0ea52fe123db0daa4c56df76d6de7
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-10-30 08:52:04 +0700

    learned basic concept of Object in Javascript

---
+++ WPU/Javascript/Basic Javascript/Object/Object.js
@@ -0,0 +1,25 @@
+var mahasiswa = {
+  nama: "Hanif Hafizhan",
+  umur: 20,
+  universitas: "ITB",
+  IPSemester: [2.78, 2.9, 3.5, 3.25],
+  IPKumulatif: function () {
+    var total = 0;
+    var ips = this.IPSemester;
+    for (var i = 0; i < ips.length; i++) {
+      //   total += this.IPSemester[i];
+      // Atau
+      total += ips[i];
+    }
+    return total / ips.length;
+  },
+  //Object didalam Object
+  alamat: {
+    jalan: "Jalan Bumi Prima Raya, Komplek Taman Bumi Prima Blok N9",
+    kota: "Cimahi",
+    kecamatan: "Cimahi Utara",
+    kelurahan: "Cibabat",
+    provinsi: "Jawa Barat",
+  },
+  jurusan: "Teknik Informatika",
+};
