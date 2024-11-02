9e77eb9307a2e55b59e5292edff3ae53a8432313
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-11-02 17:36:59 +0700

    learned DOM Manipulation (part 1)

---
+++ WPU/Javascript/Javascript DOM/DOM Manipulation/script.js
@@ -0,0 +1,24 @@
+//DOM Manipulation
+
+// changes inner content of element HTML & changes elememt's properties
+// element.innerHTML & element.style.{propertiesCSS}
+const h1 = document.getElementById('judul');
+h1.innerHTML = 'Hanif Hafizhan';
+h1.style.backgroundColor = 'lightblue';
+h1.style.color = 'coral';
+
+// changes attributes in element html
+// element.getAttribute()
+// element.setAttribute()
+// element.removeAttribute()
+// Examples:
+
+// changes class in element html
+// element.classList
+// element.classList.add()
+// element.classList.remove()
+// element.classList.toggle()
+// element.classList.item()
+// element.classList.contains()
+// element.classList.replace()
+// Examples:
