3a2ccf3ee8dbd797f4ef7199ec82333ef88a2dd9
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-01 01:55:26 +0700

    first commit for Portfolio website with tailwind css

---
+++ WPU/Portfolio Tailwind/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: `16px`,
+    },
+    extend: {
+      colors: {
+        primary: `#ff6666`,
+        secondary: `#64748b`,
+        tertiary: `#fb7185`,
+        dark: `#0f172a`,
+      },
+      screens: {
+        "2xl": `1320px`,
+      },
+    },
+  },
+  plugins: [],
+};
