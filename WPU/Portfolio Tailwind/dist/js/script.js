4c5de82b83d10bd0ccdca37c06d90d57eafd7dc0
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-01 20:29:23 +0700

    added smooth scrolling, close hamburger menu anywhere on screen,  Back to top button, and added dark-light button

--- WPU/Portfolio Tailwind/dist/js/script.js
+++ WPU/Portfolio Tailwind/dist/js/script.js
@@ -10,12 +10,38 @@ hamburger.addEventListener(`click`, function () {
 
 //  Navbar Fixed
 window.onscroll = function () {
+  const btnToTop = document.querySelector(`#backToTopBtn`);
   const header = document.querySelector(`header`);
   const fixedNav = header.offsetTop;
 
   if (window.scrollY > fixedNav) {
     header.classList.add(`navbar-fixed`);
+    btnToTop.classList.remove(`hidden`);
+    btnToTop.classList.add(`flex`);
   } else {
     header.classList.remove(`navbar-fixed`);
+    btnToTop.classList.add(`hidden`);
+    btnToTop.classList.remove(`flex`);
   }
 };
+
+// Click outside Hamburger
+window.addEventListener(`click`, function (event) {
+  if (event.target != hamburger && event.target != navMenu) {
+    hamburger.classList.remove(`hamburger-active`);
+    navMenu.classList.add(`hidden`);
+  }
+});
+
+// Darkmode Toggle
+
+const darkToggle = document.querySelector(`#dark-toggle`);
+const html = document.querySelector(`html`);
+
+darkToggle.addEventListener(`click`, function () {
+  if (darkToggle.checked) {
+    html.classList.add(`dark`);
+  } else {
+    html.classList.remove(`dark`);
+  }
+});
