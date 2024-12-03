6c4d5a84af0948b79831a2787ad0d0a42b480743
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-03 11:30:39 +0700

    Tic tac toe added time travel feature

--- WPU/React js/Tic Tac Toe Game/tic-tac-toe-project/src/App.jsx
+++ WPU/React js/Tic Tac Toe Game/tic-tac-toe-project/src/App.jsx
@@ -1,101 +1,57 @@
 /* eslint-disable react/prop-types */
 
 import { useState } from "react";
+import Board from "./components/Board";
 
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
+function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+  console.log(currentSquares);
 
-function Board() {
-  // .fill methoad to fill all array with given value
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  function handleClick(i) {
-    if (squares[i] || logicWinner(squares)) return;
-
-    // Slice with no parameter, gives entire new array
-    const nextSquares = squares.slice();
-    nextSquares[i] = xIsNext ? "X" : "O";
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+  /** @type {*}
+   * History Visualization
+   * [
+   *  [null,null,null,null,null,null,null,null,null],
+   *  ["x",null,null,null,null,null,null,null,null]
+   * ]
+   */
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
-  // Check winner
-  const winner = logicWinner(squares);
-  let status = ``;
-  winner
-    ? (status = `Winner: ${winner}`)
-    : (status = `Next Player : ${xIsNext ? "X" : "O"}`);
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description = "";
+    if (move > 0) {
+      description = `Go to move #${move}`;
+    } else {
+      description = `Go to game start`;
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
-    <>
-      <div className="status">{status}</div>
-      <div className="board">
-        <Square
-          value={squares[0]}
-          onSquareClick={() => handleClick(0)}
-        ></Square>
-        <Square
-          value={squares[1]}
-          onSquareClick={() => handleClick(1)}
-        ></Square>
-        <Square
-          value={squares[2]}
-          onSquareClick={() => handleClick(2)}
-        ></Square>
-        <Square
-          value={squares[3]}
-          onSquareClick={() => handleClick(3)}
-        ></Square>
-        <Square
-          value={squares[4]}
-          onSquareClick={() => handleClick(4)}
-        ></Square>
-        <Square
-          value={squares[5]}
-          onSquareClick={() => handleClick(5)}
-        ></Square>
-        <Square
-          value={squares[6]}
-          onSquareClick={() => handleClick(6)}
-        ></Square>
-        <Square
-          value={squares[7]}
-          onSquareClick={() => handleClick(7)}
-        ></Square>
-        <Square
-          value={squares[8]}
-          onSquareClick={() => handleClick(8)}
-        ></Square>
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-    </>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
 
-function logicWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
-      return squares[a];
-  }
-
-  return false;
-}
-
-export default Board;
+export default Game;
