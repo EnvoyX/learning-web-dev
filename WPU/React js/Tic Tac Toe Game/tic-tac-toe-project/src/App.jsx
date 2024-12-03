88db08e7ff00122bcf4021c782335d013894a136
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2024-12-03 07:45:49 +0700

    Tic Tac Tor ver.1 done

--- WPU/React js/Tic Tac Toe Game/tic-tac-toe-project/src/App.jsx
+++ WPU/React js/Tic Tac Toe Game/tic-tac-toe-project/src/App.jsx
@@ -1,32 +1,101 @@
+/* eslint-disable react/prop-types */
+
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue(`X`);
-  }
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 function Board() {
+  // .fill methoad to fill all array with given value
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    if (squares[i] || logicWinner(squares)) return;
+
+    // Slice with no parameter, gives entire new array
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  // Check winner
+  const winner = logicWinner(squares);
+  let status = ``;
+  winner
+    ? (status = `Winner: ${winner}`)
+    : (status = `Next Player : ${xIsNext ? "X" : "O"}`);
+
   return (
-    <div className="board">
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
-    </div>
+    <>
+      <div className="status">{status}</div>
+      <div className="board">
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+        ></Square>
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+        ></Square>
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+        ></Square>
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+        ></Square>
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+        ></Square>
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+        ></Square>
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+        ></Square>
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+        ></Square>
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+        ></Square>
+      </div>
+    </>
   );
+}
+
+function logicWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      return squares[a];
+  }
+
+  return false;
 }
 
 export default Board;
